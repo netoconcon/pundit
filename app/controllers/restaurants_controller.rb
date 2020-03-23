a71c82b5c0c0693850cf5a2abca136233f6b0fc3
Neto Concon <concon.neto2@gmail.com>
2020-03-23 12:28:07 -0300

    fixed controller for scafold

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -47,6 +47,6 @@ class RestaurantsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def restaurant_params
-      params.require(:restaurant).permit(:name, :user_id)
+      params.require(:restaurant).permit(:name)
     end
 end
